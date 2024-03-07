36985bdf3373c9866b9cbf3ea008b4e1b8b037dd
ninadnale <ninadnale@gmail.com>
2024-03-07 12:32:23 -0700

    MongoDB Connection

--- src/main/java/com/ninad/springpractice/movieapp/MovieappApplication.java
+++ src/main/java/com/ninad/springpractice/movieapp/MovieappApplication.java
@@ -1,13 +1,29 @@
 package com.ninad.springpractice.movieapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class MovieappApplication {
+	
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieappApplication.class, args);
+	}
+
+	@GetMapping("/root")
+	public String apiRoot() {
+		// String databaseName = env.getProperty("spring.data.mongodb.uri");
+		// System.err.println("Database Name: " + databaseName);
+		return "Welcome to the Movie App!";
 	}
 
 }
