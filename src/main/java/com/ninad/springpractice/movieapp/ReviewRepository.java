efdbefe145f8018730b99d6187e38482842404bb
ninadnale <ninadnale@gmail.com>
2024-03-07 16:31:45 -0700

    Added the Review service

---
+++ src/main/java/com/ninad/springpractice/movieapp/ReviewRepository.java
@@ -0,0 +1,15 @@
+package com.ninad.springpractice.movieapp;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+//Repositories of type interface are used to perform CRUD operations on the database
+//As this ReviewRepository extends MongoRepository, 
+//it will have all the methods to perform CRUD operations which are already implemented in MongoRepository
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId>{
+
+
+    
+}
---
+++ src/main/java/com/ninad/springpractice/movieapp/ReviewService.java
@@ -0,0 +1,28 @@
+package com.ninad.springpractice.movieapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired  //This annotation is used to inject the dependency
+    private ReviewRepository reviewRepository;  //ReviewRepository is an interface that extends MongoRepository, we will use this to perform CRUD operations on the database
+
+    @Autowired
+    private MongoTemplate mongoTemplate;    //MongoTemplate is a class that provides the implementation of the MongoOperations interface, which is used to perform CRUD operations on the database
+
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = reviewRepository.insert(new Review(reviewBody));     //inserts a new document in the database
+        
+        mongoTemplate.update(Movie.class)   //updates the movie document in the database
+            .matching(Criteria.where("imdbId").is(imdbId))  //matches the movie document with the given imdbId
+            .apply(new Update().push("reviewIds").value(review))  //pushes the review document into the reviews array of the movie document
+            .first();   //updates the first document that matches the criteria
+
+        return review;
+    }
+}
